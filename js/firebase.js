08e56b6343b672538491af1d8ef9254e14dc9cde
Kushagra963-lab <145759628+Kushagra963-lab@users.noreply.github.com>
2024-02-20 14:29:24 +0530

    Add files via upload

---
+++ js/firebase.js
@@ -0,0 +1,10 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+    apiKey: "AIzaSyAbwmZ0MaLrS-9PMxw60JJ_A_rVZZAJpsc",
+    authDomain: "blogging-website-9eda6.firebaseapp.com",
+    projectId: "blogging-website-9eda6",
+    storageBucket: "blogging-website-9eda6.appspot.com",
+    messagingSenderId: "486601420453",
+    appId: "1:486601420453:web:e45cd15b2ab672f3a4a74c"
+  };
+const app = initializeApp(firebaseConfig);
